af345e87bca27511f78c89814e2e490ecd7fb85e
Meilky <mikaeljean2k3@gmail.com>
2022-03-21 17:10:04 -0400

    Initial commit

---
+++ src/components/App.ts
@@ -0,0 +1,20 @@
+import { StateLessComponent } from "../lib/components/state-less-component";
+import { NavBar } from "./nav-bar/nav-bar";
+import { TopBar } from "./top-bar/top-bar";
+import { App as AppStyle } from "./style.module.css"
+
+export class App extends StateLessComponent {
+	constructor() {
+		super({ element: document.createElement("div") });
+
+		this.element.className = AppStyle;
+
+		this.init();
+	}
+
+	public init(): void {
+		this.children = [new TopBar(), new NavBar()];
+
+		this.appenChildren();
+	}
+}
