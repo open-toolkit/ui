c0447cec43c711838fd0e5a063280c74acc59138
Meilky <mikaeljean2k3@gmail.com>
2022-12-26 23:20:35 -0500

    Add setup script

---
+++ README.md
@@ -0,0 +1,6 @@
+# Open-Toolkit UI
+
+Please run setup first
+```sh
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+ENVPATH=.env
+
+COLORCLEAR="\e[0m"
+COLORRED="\e[1;31m"
+COLORGREEN="\e[1;32m"
+
+USE_DEFAULTS="y"
+
+read -p "Use defaults (y | n): " USE_DEFAULTS
+
+while [ "$USE_DEFAULTS" != "y" ] && [ "$USE_DEFAULTS" != "n" ]; do
+	echo -e "$COLORRED$USE_DEFAULTS is not a valid answer$COLORCLEAR"
+	read -p "Use defaults (y | n): " USE_DEFAULTS
+done
+
+function gen_env {
+	echo "Generating $ENVPATH..."
+
+	local ENV_TYPE=""
+	local ENV_TYPE_ITER="0"
+
+	if [ "$USE_DEFAULTS" == "y" ]; then
+		ENV_TYPE="development"
+	fi
+
+	while [ "$ENV_TYPE" != "development" ] && [ "$ENV_TYPE" != "production" ]; do
+		if ! [ "$ENV_TYPE_ITER" == "0" ]; then
+			echo -e "$COLORRED$ENV_TYPE is not a valid answer$COLORCLEAR"
+		fi
+
+		read -p "Env type (development | production): " ENV_TYPE
+
+		ENV_TYPE_ITER="1"
+	done
+
+	echo "Env type : $ENV_TYPE"
+	echo "ENV_TYPE=$ENV_TYPE" >> .env
+}
+
+echo "Checking if $ENVPATH exist..."
+
+if [ -f "$ENVPATH" ]; then
+	echo -e "$COLORGREEN$ENVPATH already exist$COLORCLEAR"
+else
+	echo -e "$COLORRED$ENVPATH doesn't exist$COLORCLEAR"
+	gen_env
+fi
